31a827547732cebf79495d971a048b6622373c0f
Nikita Tale <talenikita3@gmail.com>
2026-05-24 22:54:24 +0530

    Basic typo fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,18 @@
+
+
 import mongoose from "mongoose";
 
 const connectDb = async() => {
+    
     try {
         mongoose.connection.on('connected', () => console.log("Database Connected"))
         await mongoose.connect(process.env.MONGO_URI)
-    } catch (error) {
+    } 
+    
+    catch (error) {
         console.error("Database connection failed", error);
     }
 }
+
 
 export default connectDb;
